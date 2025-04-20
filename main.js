7409cc440a539d95ba9724f4dce0ee71a33dd422
shebins298 <shebins298@gmail.com>
2025-04-20 21:26:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Placeholder JS file structure
+export function analyzeMarks() {
+  console.log("Analyzing marks...");
+}
+
+export function exportPDF() {
+  console.log("Exporting PDF..."); 
+}
